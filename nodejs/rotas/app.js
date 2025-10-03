54d702a1b725f9911a607f1ee6768fc63d237fa0
Isabella Sofia Machado Coelho <isascoelhok@gmail.com>
2025-10-03 09:22:07 -0300

    versão rotas

---
+++ nodejs/rotas/app.js
@@ -0,0 +1,37 @@
+//Importa o módulo http nativo do Node.js
+import http from 'http';
+
+const PORT = 3000;
+
+//Cria o servidor
+const server = http.createServer((request, response) => {
+    const url = request.url;
+    const method = request.method;
+
+//Define o cabeçalho da resposta como HTML
+response.setHeader('Content-Type', 'text/html; charset=utf-8');
+
+//Roteamento básico
+if (url === '/'){
+    response.statusCode = 200; // OK
+    response.end('<h1>Página Inicial</h1>');
+
+} else if (url === '/sobre' && method === 'GET'){
+    response.statusCode = 200; // OK
+    response.end('<h1>Sobre Nós</h1><p>Esta é uma aplicação de exemplo com Node.js puro.</p>');
+
+} else if (url === '/contato') {
+    response.statusCode = 200 // OK
+    response.end('<h1>Fale conosoco</h1>');
+
+} else {
+    //Se nenhuma rota corresponder
+    response.statusCode = 404; // Not found
+    response.end('<h1>404 - Página não encontrada</h1>');
+}
+});
+
+//Inicia o servidor para ouvir na porta definida
+server.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
