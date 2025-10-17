ceda7eb3a9467ce5f68eae8cfffc30969770bad8
Isabella Sofia Machado Coelho <isascoelhok@gmail.com>
2025-10-17 11:09:15 -0300

    versão rotas produtos e camisetas

--- nodejs/saudacao-mvc/routes/produtos.js
+++ nodejs/saudacao-mvc/routes/produtos.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const siteController = require('../controllers/siteController');
 
-router.get('/', function(req, res, next) {
-  res.send('Você está na rota de produtos!');
-});
+router.get("/",siteController.produtos);
+router.get("/camisetas",siteController.camisetas);
 
 module.exports = router;
